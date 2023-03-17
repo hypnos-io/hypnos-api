b86b59f93636bcb57b1291af7909f9d54f6a9e86
Gabriel Lima <lima.gabrieldsantos@gmail.com>
2023-03-17 13:53:32 -0400

    add employee service

--- src/api/services/mongodb/mdb_connection.ts
+++ src/api/services/mongodb/mdb_connection.ts
@@ -3,6 +3,7 @@ import {Connection} from '../connection'
 
 export class MongoDBConnection implements Connection {
   async connect(dbURL: string): Promise<boolean> {
+    mongoose.set('strictQuery', false)
     await mongoose.connect(dbURL)
     return true
   }
--- src/domain/ports/iemployee_service.ts
+++ src/domain/ports/iemployee_service.ts
@@ -1,6 +1,16 @@
 import {ID} from '../entities/common'
 import {Employee} from '../entities/employee'
 
+export type OptionalEmployee = Employee | undefined | null
+
 export interface IEmployeeService {
-  create(newEmployee: Employee, supervisorId: ID): Promise<Employee>
+  create(newEmployee: Employee, supervisorId: ID): Promise<OptionalEmployee>
+  fetchAll(supervisorId: ID): Promise<Employee[]>
+  findById(supervisorId: ID, id: ID): Promise<OptionalEmployee>
+  update(
+    supervisorId: ID,
+    id: ID,
+    newEmployee: Partial<Employee>
+  ): Promise<OptionalEmployee>
+  deleteById(supervisorId: ID, id: ID): Promise<void>
 }
