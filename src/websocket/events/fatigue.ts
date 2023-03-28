1c673cafb409f4d630955cd5ef45148e7d3a1f7f
Gabriel Lima <lima.gabrieldsantos@gmail.com>
2023-03-27 21:22:03 -0400

    add swagger definition

--- src/websocket/events/fatigue.ts
+++ src/websocket/events/fatigue.ts
@@ -1,11 +1,15 @@
 import {Server} from 'socket.io'
+import {MongoDBConnection} from '../../api/services/mongodb/mdb_connection'
+import {MongoDBFatigueService} from '../../api/services/mongodb/mdb_fatigue_service'
 import {FatigueStatus} from '../../domain/entities/fatigue'
+import {Create} from '../../domain/use_cases/fatigue/Create'
 
 type Image = string
 
 interface SocketData {
   id: string
   employeeId: string
+  workstation: string
 }
 
 interface SocketDataRequest extends SocketData {
@@ -23,5 +27,10 @@ export function processImage(io: Server, data: SocketDataRequest) {
 
 // This function is used to get the status of the worker face from drowsy api
 export function notifyStatus(io: Server, response: SocketDataResponse) {
-  io.to(response.id).emit('notify-status', response.imageStatus)
+  const createFatigueUC = new Create(
+    new MongoDBFatigueService(new MongoDBConnection())
+  )
+  createFatigueUC.execute(response.imageStatus, response.employeeId)
+  io.to(response.id).emit('notify-status', response)
+  io.to(response.workstation).emit('notify-status', response)
 }
